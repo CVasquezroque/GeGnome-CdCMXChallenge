377402a8556dbaefa959a8e50d116692f6917cfb
CVasquezroque <35357090+CVasquezroque@users.noreply.github.com>
2020-08-15 22:42:53 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,36 @@
+let btnMenu = document.getElementById('btnmenu');
+let homemenu = document.getElementById('home-menu');
+let quemenu = document.getElementById('team-menu');
+let promenu = document.getElementById('about-menu');
+let comomenu = document.getElementById('project-menu');
+let menu = document.getElementById('menu');
+
+
+btnMenu.addEventListener('click',function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+
+
+homemenu.addEventListener('click',function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+
+quemenu.addEventListener('click',function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+
+promenu.addEventListener('click',function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+comomenu.addEventListener('click',function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+contmenu.addEventListener('click',function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
